bceae5a8a1549e63719d1ae4a2d976b5b4bbc149
Per Unneberg <per.unneberg@scilifelab.se>
2022-08-13 12:21:44 +0200

    Add subcommands

--- src/amibo/cli.py
+++ src/amibo/cli.py
@@ -1,9 +1,10 @@
-"""Console script for datasources"""
+"""Console script for amibo"""
 import logging
 import os
 import subprocess as sp
 import sys
 from argparse import ArgumentParser
+from amibo.templates import env
 
 from . import __version__
 
@@ -31,8 +32,6 @@ __RUNDOC__ = [
 
 
 def run(args):
-    if args.test:
-        args.extra_options += ["--directory", TESTDIR]
     cmd = f"snakemake -s {SNAKEFILE} {' '.join(args.extra_options)}"
     logger.info(f"running command '{cmd}'")
     try:
@@ -40,6 +39,59 @@ def run(args):
     except sp.CalledProcessError:
         logger.error(f"{cmd} failed")
         raise
+
+
+def add_run_subcommand(subparsers):
+    parser = subparsers.add_parser(
+        "run",
+        help="Run ancient microbiome snakemake workflow",
+        description="\n".join(__RUNDOC__),
+    )
+    parser.add_argument(
+        "--configfile",
+        action="store",
+        default="config/config.yaml",
+        help="configuration file",
+    )
+    parser.add_argument(
+        "--test", action="store_true", default=False, help="run small test workflow"
+    )
+    parser.set_defaults(runner=run)
+
+
+def sample_config(args):
+    template = env.get_template("amibo.yaml.j2")
+    print(template.render())
+
+def add_sample_config_subcommand(subparsers):
+    parser = subparsers.add_parser(
+        "sample_config",
+        help="Produce a sample amibo.yaml configuration file",
+        description=None
+    )
+    parser.set_defaults(runner=sample_config)
+
+
+def test(args):
+    if args.test_dir is not None:
+        print(args.test_dir)
+    args.extra_options += ["--directory", TESTDIR]
+    run(args)
+
+def add_test_subcommand(subparsers):
+    parser = subparsers.add_parser(
+        "test",
+        help="Run small test workflow",
+        description=None
+    )
+
+    parser.add_argument(
+        "--test-dir",
+        action="store",
+        default=None,
+        help="Run tests in specified directory"
+    )
+    parser.set_defaults(runner=test)
 
 
 def main(arg_list=None):
@@ -58,22 +110,9 @@ def main(arg_list=None):
 
     subparsers = top_parser.add_subparsers(dest="subcommand")
     subparsers.required = True
-
-    parser = subparsers.add_parser(
-        "run",
-        help="Run ancient microbiome snakemake workflow",
-        description="\n".join(__RUNDOC__),
-    )
-    parser.add_argument(
-        "--configfile",
-        action="store",
-        default="config/config.yaml",
-        help="configuration file",
-    )
-    parser.add_argument(
-        "--test", action="store_true", default=False, help="run small test workflow"
-    )
-    parser.set_defaults(runner=run)
+    add_run_subcommand(subparsers)
+    add_sample_config_subcommand(subparsers)
+    add_test_subcommand(subparsers)
 
     args, extra = top_parser.parse_known_args(arg_list)
 
