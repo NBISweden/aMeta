953548a1775694974488492f87dbf751063e0e88
clami66 <claudio.mirabello@scilifelab.se>
2022-10-21 11:16:00 +0200

    extract taxid with regex

--- workflow/scripts/malt_quantify_abundance.py
+++ workflow/scripts/malt_quantify_abundance.py
@@ -3,6 +3,7 @@
 
 import gzip
 from sys import argv
+import re
 
 sam = argv[1]
 ids = argv[2]
@@ -16,13 +17,14 @@ with open(ids) as id_file:
         counts[id.strip()] = 0
         taxids.append(id.strip())
 
+exp = re.compile("[|]tax[|]([0-9]+)[|]*")
+
 with gzip.open(sam, "rt") as sam_file:
     # M_ST-E00266:253:HCK7GCCXY:1:1101:20577:16463       0       AK125553.1|tax|63363    ...
     for read in sam_file:
         if "@" not in read:
-            split_read = read.split()
-            read_id = split_read[0]
-            taxid = split_read[2].split("|")[-1]
+            read_id = read.split()[0]
+            taxid = re.findall(exp, read)[0]
             if (
                 taxid in counts and (taxid, read_id) not in read_ids
             ):  # want to avoid counting duplicate reads
