bd2bb72cc0c6a048ab7537d45e6e8e3eb8fc1503
LeandroRitter <Nikolay.Oskolkov@biol.lu.se>
2024-12-16 16:08:45 +0100

    modified plot_score.R for the case of one sample, and removed the defaults channel from environment files

--- workflow/scripts/plot_score.R
+++ workflow/scripts/plot_score.R
@@ -41,7 +41,7 @@ rownames(score_matrix)<-score_matrix$ORGANISM
 score_matrix<-score_matrix[order(rownames(score_matrix)),]
 score_matrix$ORGANISM<-NULL
 score_matrix[is.na(score_matrix)]<-0
-score_matrix<-score_matrix[grepl("Homo sapiens",rownames(score_matrix))==FALSE,]
+#score_matrix<-score_matrix[grepl("Homo sapiens",rownames(score_matrix))==FALSE,]
 score_matrix
 write.table(score_matrix,file=paste0(out_dir,"/overview_heatmap_scores.txt"),col.names=TRUE,row.names=TRUE,quote=FALSE,sep="\t")
 
@@ -57,10 +57,17 @@ my_fontsize<-function(score_matrix)
 
 library("pheatmap")
 pdf(paste0(out_dir,"/overview_heatmap_scores.pdf"),paper="a4r",width=297,height=210)
+if(is.null(dim(score_matrix))==FALSE)
+{
 if(dim(score_matrix)[1]>1 & dim(score_matrix)[2]>1)
 {
   pheatmap(score_matrix, display_numbers=TRUE,fontsize=my_fontsize(score_matrix),main="Ancient microbiome profiling overview",
            cluster_rows=FALSE,cluster_cols=FALSE,number_format="%i")
+}else
+{
+  pheatmap(score_matrix, display_numbers=TRUE,fontsize=12,main="Ancient microbiome profiling overview",
+           cluster_rows=FALSE,cluster_cols=FALSE,number_format="%i",breaks=c(0,1))
+}
 }else
 {
   pheatmap(score_matrix, display_numbers=TRUE,fontsize=12,main="Ancient microbiome profiling overview",
